53ce766c0bac44944b904bf918751e4ba2e68ebe
Matheus Messias <130857698+MatheusCampagnolo@users.noreply.github.com>
2025-05-06 01:09:49 -0300

    chore(template-web): add empty routes.js and middlewares folder to project scaffold

---
+++ cli/templates/web/routes.js
@@ -0,0 +1,9 @@
+// routes.js
+// Aqui você pode definir suas rotas utilizando a função `addRoute` da Kaelum
+// Exemplo:
+
+// import { addRoute } from "kaelum";
+
+// addRoute('/', (req, res) => {
+//   res.send('Hello from Kaelum!');
+// });
