79d3ef86301c487dda23922baca1c4d8cd597920
Matheus Messias <130857698+MatheusCampagnolo@users.noreply.github.com>
2025-05-25 23:54:47 -0300

    chore: unify entrypoint to index.js and remove kaelum.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import { start } from "./core/start.js";
+import { addRoute } from "./core/addRoute.js";
+import { setMiddleware } from "./core/setMiddleware.js";
+
+export {
+  start,
+  addRoute,
+  setMiddleware
+};
--- kaelum.js
+++
@@ -1,9 +0,0 @@
-import { start } from "./core/start.js";
-import { addRoute } from "./core/addRoute.js";
-import { setMiddleware } from "./core/setMiddleware.js";
-
-export {
-  start,
-  addRoute,
-  setMiddleware
-};
